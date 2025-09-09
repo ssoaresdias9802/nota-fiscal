c2617dcba66ce803967bd1618cd80882dbd641b4
Stephanie Julia Soares Dias <ssoaresdias9802@gmail.com>
2025-09-09 16:51:15 -0300

    Create main.java
    inicio de código ( sem teste de funcionamento)

---
+++ main.java
@@ -0,0 +1,14 @@
+//Catalogo Produtos
+//int[] idsProdutos: Armazena os códigos numéricos únicos de cada produto.
+// String[] nomesProdutos: Armazena as descrições dos produtos.
+//double[] precosProdutos: Armazena os preços unitários de cada produto.
+// int[] estoquesProdutos: Armazena a quantidade disponível de cada produto. 
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int []idsProdutos = entrada.nextInt[100];
+        
+    }
